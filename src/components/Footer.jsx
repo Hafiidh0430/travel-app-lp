deca2b7a38de60dbcd03f3287fa8c1d68f4e9a1e
Hafiidh0430 <129365807+Hafiidh0430@users.noreply.github.com>
2024-08-07 10:59:53 +0700

    resize fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,13 @@ export default function Footer() {
     <>
       <footer className="footer-container text-white p-[4rem] rounded-[4rem] bg-gray-950">
         <header className="footer-header gap-4 relative flex flex-col items-center">
-          <h4 className="text-book w-[46rem] text-2xl">Let’s book today and take advantage of our services. We’re always
+          <h4 className="text-book w-[46rem] text-center text-2xl">Let’s book today and take advantage of our services. We’re always
           waiting your arrival for 24 hours, don’t miss out for all surprise of us!</h4>
           <div className="cta-btn flex gap-2 items-center">
             <a className="bg-white rounded-full px-4 py-2 text-black text-sm" href="">What a suprise?</a>
             <a className="border border-white px-4 py-2 text-sm rounded-full" href="">Book now</a>
           </div>
-          <span className="line absolute bottom-0  top-36 rounded-full w-full h-[1px] bg-white"></span>
+          <span className="line absolute bottom-0  top-[11rem] rounded-full w-full h-[1px] bg-white"></span>
         </header>
 
         <div className="footer-body h-[10rem] mt-16 flex justify-between">
